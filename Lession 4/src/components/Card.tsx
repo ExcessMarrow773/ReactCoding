55ed312eff5ff3f8981a04cb6440c77a748734e5
Atticus Walton <atticus.falkner.walton@gmail.com>
2026-01-13 14:56:04 -0600

    adds a card thing

--- Lession 4/src/components/Card.tsx
+++ Lession 4/src/components/Card.tsx
@@ -7,15 +7,26 @@ type Props = {
 
 function Card(props: Props) {
     return (
-        <div>
-            <img src={props.image} alt={props.title}/>
-            <div>
+        <div style={styles.card}>
+            <img src={props.image} alt={props.title} style={styles.image}/>
+            <div style={styles.content}>
                 <h3>{props.title}</h3>
                 <p>{props.description}</p>
                 <span>{props.date}</span>
             </div>
         </div>
     )    
+}
+
+const styles={
+    card: {
+        width: "300px",
+        borderRadius: "12px",
+        overflow: "hidden",
+        backgroundcolor: "#fff",
+    },
+    image: {},
+    content: {}
 }
 
 export default Card;
