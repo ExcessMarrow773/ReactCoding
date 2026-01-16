d857b0a1ed300fdfea987ad09317525b96ac0b8a
Atticus Walton <atticus.falkner.walton@gmail.com>
2026-01-16 17:12:17 -0600

    removed unused imports

--- Lession 6/src/pages/AboutMe.tsx
+++ Lession 6/src/pages/AboutMe.tsx
@@ -1,11 +1,9 @@
-import Nav from "../components/Nav";
 
 type Props = {};
 
 function AboutMe({}: Props) {
     return (
         <div>
-            <Nav/>
             <h1>About Me</h1>
         </div>
     )
--- Lession 6/src/pages/Home.tsx
+++ Lession 6/src/pages/Home.tsx
@@ -1,11 +1,7 @@
 import { useState } from "react";
-import Header from "../components/Header";
 import AddCards from "../components/AddCards";
 import Body from "../components/Body";
 import CardList from "../components/CardList";
-import Footer from "../components/Footer";
-import Nav from "../components/Nav";
-
 type Props = {};
 
 function Home({}: Props) {
