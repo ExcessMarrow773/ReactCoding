04e484c99f4ba5ea2d381ef4e35e8abca6d6d89a
Atticus Walton <atticus.falkner.walton@gmail.com>
2026-01-16 13:59:56 -0600

    things

--- Lession 6/src/App.tsx
+++ Lession 6/src/App.tsx
@@ -1,13 +1,24 @@
 import { Route, Routes } from "react-router"
 import Home from "./pages/Home"
 import AboutMe from "./pages/AboutMe"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Nav from "./components/Nav"
+import Body from "./components/Body"
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/about" element={<AboutMe />}/>
-    </Routes>
+    <>
+      <Header title="My Gallery Website" pagetitle='Home' />
+      <Nav/>
+      <Body>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<AboutMe />}/>
+        </Routes>
+      </Body>
+      <Footer name="deez" />
+    </>
   )
 }
 
--- Lession 6/src/components/Header.tsx
+++ Lession 6/src/components/Header.tsx
@@ -7,7 +7,6 @@ export default function Header(props: Props) {
   return (
     <>
         <h1>{props.title}</h1>
-        <h2>{props.pagetitle}</h2>
         <title>{props.pagetitle}</title>
     </>
   )
