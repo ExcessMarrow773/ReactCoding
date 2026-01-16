4fdf1d390e8f804d8723fdc8e21a384c9b8bc43e
Atticus Walton <atticus.falkner.walton@gmail.com>
2026-01-16 13:27:46 -0600

    adds AboutMe route

--- Lession 6/src/App.tsx
+++ Lession 6/src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router"
 import Home from "./pages/Home"
+import AboutMe from "./pages/AboutMe"
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<AboutMe />}
     </Routes>
   )
 }
