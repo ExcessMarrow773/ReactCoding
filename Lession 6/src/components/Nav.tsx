6c751f1c402f9677180a4056f4f4d1a5714f4da2
Atticus Walton <atticus.falkner.walton@gmail.com>
2026-01-16 13:35:05 -0600

    adds a navbar with links

---
+++ Lession 6/src/components/Nav.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router"
+
+
+function Nav() {
+  return (
+    <div>
+        <Link to="/">Home</Link>
+        <Link to="/about">About Me</Link>
+    </div>
+  )
+}
+
+export default Nav
