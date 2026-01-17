c9ccdd4fa8aa924c1ff4d5a03f79cf101b9feafb
Atticus Walton <atticus.falkner.walton@gmail.com>
2026-01-16 20:24:28 -0600

    adds some styling

--- Lession 6/src/components/Nav.tsx
+++ Lession 6/src/components/Nav.tsx
@@ -2,11 +2,19 @@ import { Link } from "react-router"
 
 
 function Nav() {
+  const myStyle = {
+    backgroundColor: "#888", // camelCase property name
+    padding: "20px",
+    borderRadius: "15px",
+    radius: "15px",
+  };
   return (
-    <div>
-        <Link to="/">Home</Link>
-        <Link to="/about">About Me</Link>
+    <>
+    
+    <div style={myStyle}>
+        <Link to="/">Home, </Link><Link to="/about">About Me</Link>
     </div>
+    </>
   )
 }
 
